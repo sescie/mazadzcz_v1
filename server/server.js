3292801b671587f022d0e641d5c91460d5c8339f
sescie <samuelsingende@gmail.com>
2025-07-19 17:27:07 +0200

    add link to prod api

--- server/server.js
+++ server/server.js
@@ -34,6 +34,17 @@ const adminRoutes = require('./routes/admin');
 app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.SERVER_PORT || 5000;
+const path = require('path');
+
+// Serve React static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+}
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
